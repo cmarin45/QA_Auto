1a9c0a0573f09b6e76ad7625036a4922950762cd
cezar45 <marin.cezar45@gmail.com>
2024-01-15 19:23:55 +0200

    added test lines

--- json api automation/tests/get_post_tests.js
+++ json api automation/tests/get_post_tests.js
@@ -1,4 +1,6 @@
 const { spec, request } = require('pactum');
+const baseUrl = 'https://jsonplaceholder.typicode.com'
+const { faker } = require('@faker-js/faker');
 
 describe('get posts test suite', ()=>{
     before(async() => {
@@ -6,7 +8,22 @@ describe('get posts test suite', ()=>{
     });
     it('test status code', async ()=>{
         await spec()
-        .get('https://jsonplaceholder.typicode.com/posts')
+        .get(`${baseUrl}/posts`)
         .expectStatus(200)
+    })
+    it('test POST', async ()=>{
+        const randomFullName = faker.person.fullName();
+        const requestBody = {
+            "userId": 2,
+            "title": randomFullName,
+            "body": "body2"
+        };
+
+        console.log(randomFullName);
+    await spec().post(`${baseUrl}/posts`)
+    .withBody(requestBody)
+    .expectStatus(201)
+    .expectJsonLike('title', requestBody.title)
+    .expectJsonLike('body', requestBody.body)
     })
 });
